1bce87e35abea8aa59948c059d0698ae3d4f2dec
wangshuai <wangshuai2@WANGSHUAI@-28>
2017-09-01 17:52:36 +0800

    test post

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var express = require('express')
 var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+var redis = require("redis");
+var client = redis.createClient(10001);
+
+client.on("error",function(err){
+    console.log("error:"+err);
+});
 
 app.get('/',function(req,res,error){
-    res.send("Hello world");
+    // res.send("Hello world");
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post('/add',urlencodedParser,function(req,res,next){
+    var order = {
+        tel : req.body.tel,
+        name : req.body.name,
+        carId : req.body.carId
+    };
+    client.sadd("order",JSON.stringify(order));
+
+    res.send("success");
 });
 
 var server = app.listen(3000,function(){
@@ -10,4 +31,4 @@ var server = app.listen(3000,function(){
     var port = server.address().port;
 
     console.log('example app listening at http://%s:%s',host,port);
-})
+});
